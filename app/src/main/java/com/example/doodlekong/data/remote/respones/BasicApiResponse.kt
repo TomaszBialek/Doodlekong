8fcdd93e9e47e34a63cc49b2b3b978d717ac3499
Tomasz Białek <bialek.tomasz225@gmail.com>
2023-08-01 17:43:51 +0200

    Retrofit setup

---
+++ app/src/main/java/com/example/doodlekong/data/remote/respones/BasicApiResponse.kt
@@ -0,0 +1,6 @@
+package com.example.doodlekong.data.remote.respones
+
+data class BasicApiResponse(
+    val successful: Boolean,
+    val message: String? = null
+)
--- app/src/main/java/com/example/doodlekong/di/AppModule.kt
+++ app/src/main/java/com/example/doodlekong/di/AppModule.kt
@@ -1,5 +1,9 @@
 package com.example.doodlekong.di
 
+import com.example.doodlekong.data.remote.api.SetupApi
+import com.example.doodlekong.util.Constants.HTTP_BASE_URL
+import com.example.doodlekong.util.Constants.HTTP_BASE_URL_LOCALHOST
+import com.example.doodlekong.util.Constants.USE_LOCALHOST
 import com.example.doodlekong.util.DispatcherProvider
 import com.google.gson.Gson
 import dagger.Module
@@ -10,6 +14,8 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -24,6 +30,17 @@ object AppModule {
                 level = HttpLoggingInterceptor.Level.BODY
             })
             .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideSetupApi(okHttpClient: OkHttpClient): SetupApi {
+        return Retrofit.Builder()
+            .baseUrl(if (USE_LOCALHOST) HTTP_BASE_URL_LOCALHOST else HTTP_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(SetupApi::class.java)
     }
 
     @Singleton
