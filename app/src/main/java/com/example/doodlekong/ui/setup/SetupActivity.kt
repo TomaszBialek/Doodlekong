b133077041fe8d820ae8333e6287b8cf5f2bd978
Tomasz Białek <bialek.tomasz225@gmail.com>
2023-08-01 17:30:20 +0200

    Setting up navigation

---
+++ app/src/main/java/com/example/doodlekong/ui/setup/SetupActivity.kt
@@ -0,0 +1,20 @@
+package com.example.doodlekong.ui.setup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.doodlekong.R
+import com.example.doodlekong.databinding.ActivitySetupBinding
+
+
+class SetupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySetupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        binding = ActivitySetupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
