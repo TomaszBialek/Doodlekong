b04044fee58a7e82d09ec564c815e8d88fe7557c
Tomasz Białek <bialek.tomasz225@gmail.com>
2023-08-09 18:02:01 +0200

    DrawingView setup

--- app/src/main/java/com/example/doodlekong/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/example/doodlekong/ui/drawing/DrawingActivity.kt
@@ -1,11 +1,15 @@
 package com.example.doodlekong.ui.drawing
 
+import android.graphics.Color
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
+import com.example.doodlekong.R
 import com.example.doodlekong.databinding.ActivityDrawingBinding
+import com.example.doodlekong.util.Constants
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 
@@ -26,11 +30,28 @@ class DrawingActivity: AppCompatActivity() {
             viewModel.checkRadioButton(checkedId)
         }
     }
+    
+    private fun selectColor(color: Int) {
+        binding.drawingView.setColor(color)
+        binding.drawingView.setThickness(Constants.DEFAULT_PAINT_THICKNESS)
+    }
 
     private fun subscribeToUiStateUpdates() {
         lifecycleScope.launchWhenStarted {
             viewModel.selectedColorButtonId.collect { id ->
                 binding.colorGroup.check(id)
+                when(id) {
+                    R.id.rbBlack -> selectColor(Color.BLACK)
+                    R.id.rbBlue -> selectColor(Color.BLUE)
+                    R.id.rbGreen -> selectColor(Color.GREEN)
+                    R.id.rbOrange -> selectColor(ContextCompat.getColor(this@DrawingActivity, R.color.orange))
+                    R.id.rbRed -> selectColor(Color.RED)
+                    R.id.rbYellow -> selectColor(Color.YELLOW)
+                    R.id.rbEraser -> {
+                        binding.drawingView.setColor(Color.WHITE)
+                        binding.drawingView.setThickness(40f)
+                    }
+                }
             }
         }
     }
